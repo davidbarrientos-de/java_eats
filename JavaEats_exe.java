2f4341e4e268e48b00c977e6277ed8eab1abbb8a
Alejandro Torres <137192433+alejandrotrrpp@users.noreply.github.com>
2023-06-24 21:19:55 +0200

    Add files via upload

---
+++ JavaEats_exe.java
@@ -0,0 +1,13 @@
+package poo.java_eatsEXE;
+
+import poo.java_eatsGrafica.Login;
+
+
+
+public class JavaEats_exe {
+    public static void main (String[] args){
+        Login log = new Login();
+        log.setVisible(true);
+        log.setLocationRelativeTo(null);
+    }
+}
